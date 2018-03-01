53dfa9588f3fa32724f024b568355121e2eda21b
Olecika Darii <olealea001@gmail.com>
2018-03-01 23:32:15 +0200

    practice 04_1

--- exercise/ShoppingCart.java
+++ exercise/ShoppingCart.java
@@ -2,18 +2,16 @@ package exercise;
 
 public class ShoppingCart {
     public static void main(String[] args){
-
-
-
         String custName = "Mary Smith";
         String itemDesc = "Shirt";
         String message;
+        double price = 2;
+        double tax = 0.2;
+        int quantity = 5;
+        double total = price * tax * quantity;
 
-        message = custName + " wants to purchase a " + itemDesc;
-
-
-        System.out.println("Welcome to the Shopping Cart!");
-
+        message = custName +" wants to purchase " +quantity+" " +itemDesc;
         System.out.println(message);
+        System.out.println("Total cost is "+total+" lei.");
     }
 }
