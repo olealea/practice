2b924b315bda619aacc7970bdf048fcd93984a19
Olecika Darii <olealea001@gmail.com>
2018-03-01 23:11:41 +0200

    practice 04_1

--- exercise/ShoppingCart.java
+++ exercise/ShoppingCart.java
@@ -4,6 +4,16 @@ public class ShoppingCart {
     public static void main(String[] args){
 
 
+
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message;
+
+        message = custName + " wants to purchase a " + itemDesc;
+
+
         System.out.println("Welcome to the Shopping Cart!");
+
+        System.out.println(message);
     }
 }
